7eafa4665d88f5e075e7ef1ba141af2ddf9060e5
Arun Tejasvi Chaganty <arunchaganty@gmail.com>
2018-06-01 11:58:41 -0700

    Updated paths to data files

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.0.1',
+    version='0.0.2',
 
     description='Python-based tool to quickly view data',
     long_description=long_description,
@@ -90,7 +90,9 @@ setup(
     # If there are data files included in your packages that need to be
     # installed, specify them here.  If using Python 2.6 or less, then these
     # have to be included in MANIFEST.in as well.
+    package_dir={'fastex': 'fastex'},
     package_data={
+        'fastex': ['view.html', 'template.html'],
     },
 
     # Although 'package_data' is the preferred approach, in some case you may
